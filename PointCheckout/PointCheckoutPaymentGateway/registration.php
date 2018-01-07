c23111393513dca2271e868b6b40cd295ce06699
yaser safou <yaser.safou@gmail.com>
2018-01-07 17:44:56 +0200

    renaming to PointCheckout as a vendor directory

---
+++ PointCheckout/PointCheckoutPaymentGateway/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2017 PointCheckout. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PointCheckout_PointCheckoutPaymentGateway',
+    __DIR__
+);
